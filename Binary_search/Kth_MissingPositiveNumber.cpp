1aca9de942d97ee3409ba05ca9da3d3d6362b485
RATNESH MAURYA <ratneshmaurya2001@gmail.com>
2022-05-09 23:46:20 +0530

    updated kth missing positive number

--- Binary_search/Kth_MissingPositiveNumber.cpp
+++ Binary_search/Kth_MissingPositiveNumber.cpp
@@ -20,7 +20,12 @@ public:
 
 
 // O(N) solution -----
-// 
+// assume 'k' is pointing to the answer that is kth missing num,
+// if arr(i) becomes greater than 'k' then simply return value
+//  of k as answer else if (arr[i] <= k) then do k++; and iterate krte rho for 
+// next value of array,,,, k++ issiliye kiya becoz hme 5th missing chahiye than
+//  and we got an element that is not missing hence we have to increase the 
+// value of k so that 5th missing prr hi point kre rhe 'k'.
 // 
 // class Solution {
 // public:
