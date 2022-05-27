ed5d4d72a7fb987c17d2eed0f047b8fec64ebcfc
RATNESH MAURYA <ratneshmaurya2001@gmail.com>
2022-05-28 00:31:52 +0530

    rotate Matrix By 90 degree

---
+++ 2D_matrix/rotateMatrixBy90.cpp
@@ -0,0 +1,37 @@
+
+
+//IDEA===  We first transpose the given matrix, and then reverse the content of individual rows to
+//  get the resultant 90 degree clockwise rotated matrix.
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) 
+    {
+        int r=matrix.size();
+        int c=matrix[0].size();
+        
+        //first doing transpose
+        for(int i=0;i<r;i++){
+            for(int j=i;j<c;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        
+        //now doing reverse of each row
+        int left,right;
+        for(int i=0;i<r;i++){
+            left=0,right=c-1; // using two pointer theorem like in 1D array for reversing each row.
+            while(left<right){
+                swap(matrix[i][left],matrix[i][right]);
+                left++;
+                right--;
+            }
+        }
+
+        // you can simply write second for loop also as (just in two lines) ---- does same work in same time complexity
+        // for(int i=0;i<r;i++){
+        //     reverse(matrix[i].begin(),matrix[i].end());
+        // }
+    }
+};
+
