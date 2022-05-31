246d90c118377842ae355001b296a79b20bc271e
RATNESH MAURYA <ratneshmaurya2001@gmail.com>
2022-05-31 20:42:49 +0530

    AllDuplicatesInString

---
+++ Strings/AllDuplicatesInString.cpp
@@ -0,0 +1,18 @@
+
+// using maps
+// in gfg in all methods used space thus have time -O(N) and space-O(num of chars);
+#include <bits/stdc++.h>
+using namespace std;
+void printDups(string str)
+{
+    map<char, int> count;
+    for (int i = 0; i < str.length(); i++) {
+        count[str[i]]++;
+    }
+ 
+    for (auto it : count) {
+        if (it.second > 1)
+            cout << it.first << ", count = " << it.second
+                 << "\n";
+    }
+}
