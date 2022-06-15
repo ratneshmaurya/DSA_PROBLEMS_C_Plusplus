72c1ec790533ca3e85a8f9f85b5ae68b84ff3514
RATNESH MAURYA <ratneshmaurya2001@gmail.com>
2022-06-15 17:41:20 +0530

    common_elements_In_3arrays

--- Array/common_elements_In_3arrays.cpp
+++ Array/common_elements_In_3arrays.cpp
@@ -1,3 +1,53 @@
+
+// O(n1+n2+n3) time
+// O(1) space------------
+// --------------------------------------------------------------------------
+
+class Solution
+{
+    public:    
+       vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
+        {
+            int i=0,j=0,k=0; 
+            vector<int>ans;
+            while(i<n1 && j<n2 && k<n3){
+                
+                if(A[i]==B[j] && B[j]==C[k]){
+                    ans.push_back(A[i]);
+                    i++;k++;j++;
+                }
+                else if(A[i]<B[j]) i++;
+                
+                else if(B[j]<C[k]) j++;
+                
+                else k++;
+                
+                // to move steps for preventing duplicates elements
+                int a=A[i-1];
+                while(A[i]==a)i++;
+                int b=B[j-1];
+                while(B[j]==b)j++;
+                int c=C[k-1];
+                while(C[k]==c)k++;
+            }
+            
+            if(ans.size()==0) return {-1};
+            
+            else return ans;
+        }
+
+};
+
+
+
+// ------------------------------------------------------------------------
+// ------------------------------------------------------------------------
+
+// O(n1+n2+n3) time
+// O(n1+n2+n3) space
+// --------------------------------------------------------------------------
+
+
 class Solution
 {
     public:    
