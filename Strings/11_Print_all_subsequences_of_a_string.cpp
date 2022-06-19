cdb2b4c2982d3e816133157bb8a9e2eba2ce5c7f
RATNESH MAURYA <ratneshmaurya2001@gmail.com>
2022-06-19 20:36:29 +0530

    Strings/11_Print_all_subsequences_of_a_string

--- Strings/11_Print_all_subsequences_of_a_string.cpp
+++ Strings/11_Print_all_subsequences_of_a_string.cpp
@@ -1,3 +1,28 @@
+
+
+//better approach
+
+void printSubsequence(string input, string output)
+{
+    
+    // if the input is empty print the output string
+    if (i==input.length()) {
+        cout << output << endl;
+        return;
+    }
+ 
+    //include character and then pass
+    printSubsequence(input,i+1, output + input[0]);
+ 
+    //exclude character and then pass
+    printSubsequence(input,i+1, output);
+}
+
+
+
+
+//in below approach we are using substr , it has time complexity O(N) hence donot use.
+
 void printSubsequence(string input, string output)
 {
     
