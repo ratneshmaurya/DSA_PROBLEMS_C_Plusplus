6462eb4db44959606effd3f00ecaf33bb331b595
RATNESH MAURYA <ratneshmaurya2001@gmail.com>
2022-06-21 12:11:27 +0530

    permutation_of_string

---
+++ Strings/12_permutation_of_string.cpp
@@ -0,0 +1,27 @@
+
+class Solution
+{
+    private:
+        void helper(string S,set<string>&output,int l,int r){
+            if(l==r){
+              output.insert(S);
+            }
+            else{
+                for(int i=l;i<=r;i++){
+                    swap(S[i],S[l]);
+                    helper(S,output,l+1,r);
+                    swap(S[i],S[l]);
+                }
+            }
+        }
+	public:
+		vector<string>find_permutation(string S)
+		{
+		    set<string>output;  // becoz on gfg to avoid duplicate outputs and have output in ascending order.
+		    helper(S,output,0,S.size()-1);
+		    
+		  //  now as we have to return the vector hence making vector from set;
+		    vector<string>v(output.begin(),output.end());
+		    return v;
+		}
+};
