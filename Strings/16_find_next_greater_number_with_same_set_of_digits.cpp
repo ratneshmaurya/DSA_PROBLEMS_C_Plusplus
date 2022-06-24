e3c05bdad4badbe90182f4204c4201f0b2a58fd4
RATNESH MAURYA <ratneshmaurya2001@gmail.com>
2022-06-24 12:43:05 +0530

    Strings/16_find_next_greater_number_with_same_set_of_digits

---
+++ Strings/16_find_next_greater_number_with_same_set_of_digits.cpp
@@ -0,0 +1,43 @@
+
+
+// first of all, it is same as next permutation of a number 
+class Solution{
+private:
+        int findNextGreater(vector<int>& nums,int index,int pivotIndex)
+        {
+            int justLarge=pivotIndex;
+            for(int i=pivotIndex;i<nums.size();i++){
+                
+//                 <= needed hai
+                if(nums[i]>nums[index] && nums[i]<=nums[justLarge]){
+                    justLarge=i;
+                }
+            }
+            return justLarge;
+        }
+    
+        void reverse(vector<int>& nums,int index){
+            int left=index,right=nums.size()-1;
+            while(left<right){
+                swap(nums[left++],nums[right--]);
+            }
+        }
+public:
+    vector<int> nextPermutation(int N, vector<int>nums){
+        int i,nextIndex=-1;
+        for(i=nums.size()-1;i>0;i--){
+            if(nums[i]>nums[i-1]){
+                nextIndex=findNextGreater(nums,i-1,i);
+                swap(nums[nextIndex],nums[i-1]);
+                break;
+            }
+        }
+        if(nextIndex==-1){
+            reverse(nums,0);
+        }
+        else{
+             reverse(nums,i);
+        }
+        return nums;
+    }
+};
