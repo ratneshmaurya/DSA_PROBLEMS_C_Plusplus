98f11852198140e5a7937d3dc62385d596439908
RATNESH MAURYA <ratneshmaurya2001@gmail.com>
2022-07-15 13:30:49 +0530

    Stack/3_previous_greater.cpp

---
+++ Stack/3_previous_greater.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+    public:
+    //Function to calculate the span of stockâ€™s price for all n days.
+    vector <int> calculateSpan(int price[], int n)
+    {
+       // Your code here
+       stack<int>s;
+       vector<int>ans;
+       s.push(INT_MAX);  //making this so as to compare it as last element of stack
+       if(n==0) return ans;  //returning empty vector if size==0
+       
+       for(int i=0;i<n;i++){
+           while(price[i]>=s.top()) s.pop();
+           
+           //if saare elements nikl jaae, means all number left of this index are smaller
+           if(s.top()==INT_MAX) {
+               ans.push_back(-1);
+           }
+           //if saare elements na nikle stack se, means there is a number which is greater found
+           else{
+               ans.push_back(s.top());
+           }
+           
+           //at last push this element with index in stack
+           s.push(price[i]);
+       }
+       return ans;
+    }
+};
